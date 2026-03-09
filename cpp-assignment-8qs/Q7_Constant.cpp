f1102496f0b1f3e343826acdf75b308dbdfc1f4c
rajdeepgirkar <rajdeepgirkar950@gmail.com>
2026-03-09 23:54:22 +0530

    constant in functions

---
+++ cpp-assignment-8qs/Q7_Constant.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const float PI = 3.14159f;
+
+float circleArea(float radius);
+float circlePerimeter(float radius);
+
+int main() {
+    float radius = 7.0f;
+
+    cout << fixed << setprecision(4);
+
+    cout << "Radius = " << radius << endl;
+    cout << "Area = " << circleArea(radius) << endl;
+    cout << "Perimeter = " << circlePerimeter(radius) << endl;
+
+    // PI = 3.0f;   // attempt to modify const variable
+
+    return 0;
+}
+
+float circleArea(float radius) {
+    return PI * radius * radius;
+}
+
+float circlePerimeter(float radius) {
+    return 2 * PI * radius;
+}
+
+//  error: assignment of read-only variable 'PI'
+//    20 |     PI = 3.0f;   // attempt to modify const variable
+//       |     ~~~^~~~~~
