c5ee5c55b4307d931f2a80df49cdd76320b9d1af
rajdeepgirkar <rajdeepgirkar950@gmail.com>
2026-03-27 16:49:56 +0530

    print data

---
+++ Assignment_01/Q1_primitive_dt.java
@@ -0,0 +1,23 @@
+public class Q1_primitive_dt{
+    public static void main(String[] args) {
+        
+        int i = 10;
+        float f = 10.5f;
+        double d = 10.9999;
+        char c = 'A';
+        boolean bool = true;
+        long l = 100000L;
+        short s = 200;
+        byte b = 10;
+        
+        System.out.println("Int = " + i);
+        System.out.println("Float = " + f);
+        System.out.println("Double = " + d);
+        System.out.println("Char = " + c);
+        System.out.println("Boolean = " + bool);
+        System.out.println("Long = " + l);
+        System.out.println("Short = " + s);
+        System.out.println("Byte = " + b);
+
+    }
+}
