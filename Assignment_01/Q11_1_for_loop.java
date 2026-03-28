3a870e672fe5f2255ce666acc759d828d2e2ad0a
rajdeepgirkar <rajdeepgirkar950@gmail.com>
2026-03-28 21:34:23 +0530

    print 1 to 50

---
+++ Assignment_01/Q11_1_for_loop.java
@@ -0,0 +1,7 @@
+public class Q11_1_for_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Assignment_01/Q11_2_while_loop.java
@@ -0,0 +1,10 @@
+public class Q11_2_while_loop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
