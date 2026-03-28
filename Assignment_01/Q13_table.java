6762e61cf0edccca7b5d22c5e360ea18f77f81cc
rajdeepgirkar <rajdeepgirkar950@gmail.com>
2026-03-28 21:57:03 +0530

    table

---
+++ Assignment_01/Q13_table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q13_table {
+    public static void printMultiplicationTable(int n) {
+        int result;
+        for (int i = 1; i <= 10; i++) {
+            result = n*i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        printMultiplicationTable(num);
+    }
+}
